2095936a4276718290196dd22c9d594a479d8dde
pratyushghanta <pratyusha.g1117@gmail.com>
2024-06-23 21:20:26 +0530

    prepare base model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.ChickenDiseaseCNNClassifier.logging import logger
 from src.ChickenDiseaseCNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from src.ChickenDiseaseCNNClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -12,3 +12,17 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "Prepare Base Model"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<\n\nx======================x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
