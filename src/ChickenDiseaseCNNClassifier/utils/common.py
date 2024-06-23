224997355c0bb1430bf48b59f530da0e2f69e6ca
pratyushghanta <pratyusha.g1117@gmail.com>
2024-06-23 10:53:52 +0530

    data ingestion configured

--- src/ChickenDiseaseCNNClassifier/utils/common.py
+++ src/ChickenDiseaseCNNClassifier/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from ChickenDiseaseCNNClassifier import logger
+from ChickenDiseaseCNNClassifier.logging import logger
 import json
 import joblib
 from ensure import ensure_annotations
